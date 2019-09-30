766a8787d76b454e8506f88dc0d5048345c2d22a
VilayD <55212942+VilayD@users.noreply.github.com>
2019-09-29 22:56:16 -0600

    Second JavaScript Assignment

---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,13 @@
+
+function display () {
+    var str = "Hello! How are you?";
+    var result = str.fontcolor("blue");
+    document.getElementById("click").innerHTML = result;
+}
+
+function myFunction() {
+    var sentence = "I am always learning,";
+    sentence += " and there is a lot to learn!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+    
