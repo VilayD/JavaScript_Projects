08b921dd1182312fdb9e84cb08a65ff350d33983
VilayD <55212942+VilayD@users.noreply.github.com>
2019-09-29 20:39:50 -0600

    First Assignment
    Basic JavaScript

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,21 @@
+
+
+var A="Hello! How can I help you today?";
+window.alert(A);
+
+var B = "Mom said, \"You are to do the dishes." + " Make sure it is all put away and don\'t make a mess.\""
+document.write(B)
+
+var Family = " The Davis\'", Dad = "Shane", Mom = "Phan", Kid1 = "Ronin", Kid2 = "Kenji", Kid3 = "Aixa";
+
+var Family = Family.fontcolor("red");
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("babyblue");
+var Kid1 = Kid1.fontcolor("purple");
+var Kid2 = Kid2.fontcolor("pink");
+var Kid3 = Kid3.fontcolor("babypink");
+
+
+document.write(Family);
+
+document.write(5 + 5);
